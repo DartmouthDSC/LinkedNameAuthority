03c6a80b53a591e943f0187ad49fd93a0a6c1009
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-05-20 16:43:01 -0400

    Fixed foaf attribute to be img, not image.

--- app/models/lna_person.rb
+++ app/models/lna_person.rb
@@ -7,7 +7,7 @@ class LnaPerson < ActiveFedora::Base
   property :given_name, predicate: ::RDF::FOAF.givenName, multiple: false
   property :family_name, predicate: ::RDF::FOAF.familyName, multiple: true
   property :title, predicate: ::RDF::FOAF.title, multiple: true
-  property :image, predicate: ::RDF::FOAF.image, multiple: false
+  property :image, predicate: ::RDF::FOAF.img, multiple: false
   property :mbox, predicate: ::RDF::FOAF.mbox, multiple: false
   property :mbox_sha1sum, predicate: ::RDF::FOAF.mbox_sha1sum, multiple: false
   property :homepage, predicate: ::RDF::FOAF.homepage, multiple: false
