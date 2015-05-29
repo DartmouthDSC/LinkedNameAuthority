6c1a9b306a87af37d3f1b6bb376c54d8a624863d
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-05-29 09:58:27 -0400

    Added prefix to each field signifiying with ontology its in. Created an ad-hoc time ontology in the LnaAppointment model.

--- app/models/lna_account.rb
+++ app/models/lna_account.rb
@@ -1,10 +1,10 @@
 class LnaAccount < ActiveFedora::Base
 
-  belongs_to :LnaPerson
+  belongs_to :LnaPerson, predicate: ActiveFedora::RDF::Fcrepo::RelsExt.isPartOf
   
-  property :title, predicate: ::RDF::DC.title, multiple: false
-  property :online_account, predicate: ::RDF::FOAF.onlineAccount, multiple: false
-  property :account_name, predicate: ::RDF::FOAF.accountName, multiple: false
-  property :account_service_homepage, ::RDF::FOAF.accountServiceHomepage, multiple: false
+  property :dc_title, predicate: ::RDF::DC.title, multiple: false
+  property :foaf_online_account, predicate: ::RDF::FOAF.OnlineAccount, multiple: false
+  property :foaf_account_name, predicate: ::RDF::FOAF.accountName, multiple: false
+  property :foaf_account_service_homepage, predicate: ::RDF::FOAF.accountServiceHomepage, multiple: false
 
 end
--- app/models/lna_appointment.rb
+++ app/models/lna_appointment.rb
@@ -1,14 +1,20 @@
 class LnaAppointment < ActiveFedora::Base
-  belongs_to :LnaPerson
+  belongs_to :lna_person,
+             predicate: ActiveFedora::RDF::Fcrepo::RelsExt.isPartOf
+
+  owltime = ::RDF::Vocabulary.new('http://www.w3.org/2006/time/')
   
-  property :title, predicate: ::RDF::VCARD.title, multiple: false
-  property :email, predicate: ::RDF::VCARD.email, multiple: false
-  property :org, predicate: ::RDF::VCARD.org, multiple: false
-  property :street_address, predicate: ::RDF::VCARD.street-address, multiple: false
-  property :pobox, predicate: ::RDF::VCARD.pobox, multiple: false
-  property :locality, predicate: ::RDF::VCARD.locality, multiple: false
-  property :postal_code, predicate: ::RDF::VCARD.postal-code, multiple: false
-  property :country_name, predicate: ::RDF::VCARD.country-name, multiple: false
-  property :has_beginning, predicate: ::RDF::XSD.dateTimeStamp, multiple: false
-  property :has_end, predicate: ::RDF::XSD.dateTimeStamp, multiple: false
+  property :vcard_title, predicate: ::RDF::VCARD.title, multiple: false
+  property :vcard_email, predicate: ::RDF::VCARD.email, multiple: false
+  property :vcard_org, predicate: ::RDF::VCARD.org, multiple: false
+  property :vcard_street_address, predicate: ::RDF::VCARD['street-address'],
+           multiple: false
+  property :vcard_pobox, predicate: ::RDF::VCARD['post-office-box'], multiple: false
+  property :vcard_locality, predicate: ::RDF::VCARD.locality, multiple: false
+  property :vcard_postal_code, predicate: ::RDF::VCARD['postal-code'], multiple: false
+  property :vcard_country_name, predicate: ::RDF::VCARD['country-name'],
+     multiple: false
+  
+  property :time_has_beginning, predicate: owltime.hasBeginning, multiple: false
+  property :time_has_end, predicate: owltime.hasEnd, multiple: false
 end
