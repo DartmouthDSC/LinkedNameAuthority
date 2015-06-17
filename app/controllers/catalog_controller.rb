583ba0cc673ebbf685565a3af2a759d3915c9644
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-06-17 15:53:26 -0400

    Added configuration to catalog controller for lna models. Added template to display associated accounts and appointments when displaying a person.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,23 +5,33 @@ class CatalogController < ApplicationController
 
   include Hydra::Catalog
   # These before_filters apply the hydra access controls
-  before_filter :enforce_show_permissions, :only=>:show
+  # before_filter :enforce_show_permissions, :only=>:show
   # This applies appropriate access controls to all solr queries
-  CatalogController.search_params_logic += [:add_access_controls_to_solr_params]
+  # CatalogController.search_params_logic += [:add_access_controls_to_solr_params]
 
+  before_filter :decode_id
+  
+  # Method to decode fedora id's that have slashes in them.
+  def decode_id
+    params[:id] = CGI.unescape(params[:id]) if params[:id]
+  end
 
   configure_blacklight do |config|
     config.search_builder_class = Hydra::SearchBuilder
     config.default_solr_params = {
+      :qf => 'dc_netid_tesim foaf_name_tesim foaf_given_name_tesim foaf_family_name_tesim foaf_title_tesim vcard_title_tesim vcard_org_tesim time_has_beginning_dtsim time_has_end_dtism dc_title_tesim foaf_account_name_tesim foaf_account_service_homepage_tesim',
       :qt => 'search',
       :rows => 10
     }
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_tesim'
-    config.index.display_type_field = 'has_model_ssim'
+    config.index.title_field = ['foaf_name_tesim', 'dc_title_tesim', 'vcard_title_tesim']
+    config.index.display_type_field = 'active_fedora_model_ssi'
 
 
+    # solr field configuration for show page
+    config.show.display_type_field = 'active_fedora_model_ssi'
+    
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
     #
@@ -58,34 +68,52 @@ class CatalogController < ApplicationController
 
 
     # solr fields to be displayed in the index (search results) view
-    #   The ordering of the field names is the order of the display
-    config.add_index_field solr_name('title', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_index_field solr_name('title_vern', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_index_field solr_name('author', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_index_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_index_field solr_name('format', :symbol), :label => 'Format:'
-    config.add_index_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_index_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_index_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_index_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
+    # The ordering of the field names is the order of the display
+    # LnaPerson
+    config.add_index_field solr_name('foaf_title', :stored_searchable), label: 'Title'
+    config.add_index_field solr_name('foaf_name', :stored_searchable), label: 'Name'
+    # LnaAccount
+    config.add_index_field solr_name('dc_title', :stored_searchable), label: 'Title'
+    config.add_index_field solr_name('foaf_account_name', :stored_searchable), label: 'Account Name'
+    # LnaAppointment
+    config.add_index_field solr_name('vcard_title', :stored_searchable), label: 'Title'
+    config.add_index_field solr_name('vcard_org', :stored_searchable), label: 'Org'
+    config.add_index_field solr_name('time_has_beginning', :dateable), label: 'Begins'
+    config.add_index_field solr_name('tims_has_end', :dateable), label: 'Ends'
+    
 
     # solr fields to be displayed in the show (single result) view
-    #   The ordering of the field names is the order of the display
-    config.add_show_field solr_name('title', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_show_field solr_name('title_vern', :stored_searchable, type: :string), :label => 'Title:'
-    config.add_show_field solr_name('subtitle', :stored_searchable, type: :string), :label => 'Subtitle:'
-    config.add_show_field solr_name('subtitle_vern', :stored_searchable, type: :string), :label => 'Subtitle:'
-    config.add_show_field solr_name('author', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_show_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:'
-    config.add_show_field solr_name('format', :symbol), :label => 'Format:'
-    config.add_show_field solr_name('url_fulltext_tsim', :stored_searchable, type: :string), :label => 'URL:'
-    config.add_show_field solr_name('url_suppl_tsim', :stored_searchable, type: :string), :label => 'More Information:'
-    config.add_show_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_show_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_show_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_show_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
-    config.add_show_field solr_name('isbn', :stored_searchable, type: :string), :label => 'ISBN:'
+    # The ordering of the field names is the order of the display
+    # LnaPerson
+    config.add_show_field solr_name('foaf_title', :stored_searchable), label: 'Title'
+    config.add_show_field solr_name('foaf_name', :stored_searchable), label: 'Name'
+    config.add_show_field solr_name('foaf_given_name', :stored_searchable), label: 'Given Name'
+    config.add_show_field solr_name('foaf_family_name', :stored_searchable), label: 'Family Name'
+    config.add_show_field solr_name('dc_netid', :stored_searchable), label: 'NetID'
+    config.add_show_field solr_name('foaf_image', :displayable), label: 'Image'
+    config.add_show_field solr_name('foaf_mbox', :displayable), label: 'MBox'
+    config.add_show_field solr_name('foaf_homepage', :displayable), label: 'Homepage'
+    config.add_show_field solr_name('foaf_publications', :displayable), label: 'Publications'
+    config.add_show_field solr_name('foaf_workplace_homepage', :displayable), label: 'Workplace Homepage'
+    # LnaAppointment
+    config.add_show_field solr_name('vcard_title', :stored_searchable), label: 'Title'
+    config.add_show_field solr_name('vcard_email', :stored_searchable), label: 'Email'
+    config.add_show_field solr_name('vcard_org', :stored_searchable), label: 'Org'
+    config.add_show_field solr_name('vcard_street_address', :displayable), label: 'Street Address'
+    config.add_show_field solr_name('vcard_pobox', :displayable), label: 'P.O. Box'
+    config.add_show_field solr_name('vcard_locality', :displayable), label: 'Locality'
+    config.add_show_field solr_name('vcard_postal_code', :displayable), label: 'Postal Code'
+    config.add_show_field solr_name('vcard_country_name', :displayable), label: 'Country'
+    config.add_show_field solr_name('time_has_beginning', :dateable), label: 'Begins'
+    config.add_show_field solr_name('time_has_end', :dateable), label: 'Ends'
+    # Lna Account
+    config.add_show_field solr_name('dc_title', :stored_searchable), label: 'Title'
+    config.add_show_field solr_name('foaf_online_account', :displayable), label: 'Online Account'
+    config.add_show_field solr_name('foaf_account_name', :stored_searchable), label: 'Account Name'
+    config.add_show_field solr_name('foaf_account_service_homepage', :stored_searchable), label: 'Account Service Homepage'
 
+    config.add_show_field 'isDependentOf_ssim', label: 'Belongs to'
+    
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
     #
@@ -95,7 +123,7 @@ class CatalogController < ApplicationController
     # that specifies the same :qt as default for our own internal
     # testing purposes.
     #
-    # The :key is what will be used to identify this BL search field internally,
+    # The :key is what will be used to idnentify this BL search field internally,
     # as well as in URLs -- so changing it after deployment may break bookmarked
     # urls.  A display label will be automatically calculated from the :key,
     # or can be specified manually to be different.
@@ -106,26 +134,25 @@ class CatalogController < ApplicationController
 
     config.add_search_field 'all_fields', :label => 'All Fields'
 
-
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
-    config.add_search_field('title') do |field|
+    config.add_search_field('netid') do |field|
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
       field.solr_local_parameters = {
-        :qf => '$title_qf',
-        :pf => '$title_pf'
+        :qf => '$dc_netid_qf',
+        :pf => '$dc_netid_pf'
       }
     end
 
-    config.add_search_field('author') do |field|
+    config.add_search_field('name') do |field|
       field.solr_local_parameters = {
-        :qf => '$author_qf',
-        :pf => '$author_pf'
+        :qf => '$foaf_name_qf',
+        :pf => '$foaf_name_pf'
       }
     end
 
@@ -153,7 +180,5 @@ class CatalogController < ApplicationController
     # mean") suggestion is offered.
     config.spell_max = 5
   end
-
-
 
 end
