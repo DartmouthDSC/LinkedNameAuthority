86d023862e48ba03f13092e6921e4ed3fb5acedd
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-09-01 14:57:50 -0400

    Ran mailer generator for ImporterMailer.

---
+++ app/mailers/importer_mailer.rb
@@ -0,0 +1,2 @@
+class ImporterMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/importer_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/importer_mailer
+class ImporterMailerPreview < ActionMailer::Preview
+
+end
