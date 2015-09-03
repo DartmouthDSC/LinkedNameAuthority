323308616e19025c9e40ab5c9530b7e3c63d4526
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-09-03 15:34:20 -0400

    Added cron job for oracle faculty load. All output from cron jobs are logged and errors are emailed out.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,15 @@
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+
 # Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+env :MAILTO, 'carlamgalarza@gmail.com'
 
-# Learn more: http://github.com/javan/whenever
+CRON_LOG = "#{Rails.root}/log/cron.log"
+
+# All output is written to cron.log and errors are emailed out.
+set :output, lambda { "2>&1 >> #{CRON_LOG} | tee --append  #{CRON_LOG} " }
+
+# Run oracle-faculty load once a day.
+every 1.day, :at => '3:00 am' do
+  rake "import:oracle-faculty"
+end
