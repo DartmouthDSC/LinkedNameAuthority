5edf5ef2d43f0dc60b5bd0354b569bce44a14dbf
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-09-08 15:47:02 -0400

    Using type validator instead of validator method in class.

--- app/models/lna/account.rb
+++ app/models/lna/account.rb
@@ -13,7 +13,8 @@ module Lna
     validates_presence_of :account_holder, :title, :online_account,
                           :account_name, :account_service_homepage
 
-    validate :account_holder_type
+    # Validation to assure account_holder is a Lna::Person or Lna::Organization.
+    validates :account_holder, type: { valid_types: [Lna::Person, Lna::Organization] }
   
     property :title, predicate: ::RDF::DC.title, multiple: false do |index|
       index.as :stored_searchable
@@ -29,15 +30,6 @@ module Lna
 
     property :account_service_homepage, predicate: ::RDF::FOAF.accountServiceHomepage, multiple: false do |index|
       index.as :stored_searchable # Should not be added to the search query!
-    end
-
-    # Validation to assure account_holder is a Lna::Person or
-    # Lna::Organization.
-    def account_holder_type
-      unless(account_holder.is_a?(Lna::Person) ||
-             account_holder.is_a?(Lna::Organization))
-        errors.add(:account_holder, 'must be a Lna::Person or Lna::Organization')
-      end
     end
   end
 end
