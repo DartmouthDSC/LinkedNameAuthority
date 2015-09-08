5103f0571b441338fbd163b2e8a38b0eec3e911f
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-09-08 15:46:03 -0400

    Using Lna::OrganizationCoreBehavior in both active and historic organization. Active and historic organization have a good part of their individual features. ChangeEvent changed to reflect new model and use the new organization classes.

--- app/models/lna/organization/change_event.rb
+++ app/models/lna/organization/change_event.rb
@@ -1,27 +1,35 @@
 module Lna
   class Organization
     class ChangeEvent < ActiveFedora::Base
-      has_many :resulting_organizations, class_name: 'Lna::Organization',
+      has_many :resulting_organizations, class_name: 'ActiveFedora::Base',
                predicate: ::RDF::Vocab::ORG.resultingOrganization,
                as: 'resulted_from'
-      has_many :original_organizations, class_name: 'Lna::Organization',
+      has_many :original_organizations, class_name: 'Lna::Organization::Historic',
                predicate: ::RDF::Vocab::ORG.originalOrganization,
                as: 'changed_by'
-
+      
       validate :max_one_original_org
 
+      # Resulting organization must be a Lna::Organization or Lna::Organization::Historic
+      validates :resulting_organizations,
+               type: { valid_types: [Lna::Organization, Lna::Organization::Historic] }
+                                                                    
       validates_presence_of :resulting_organizations, :original_organizations,
                             :at_time, :description
   
-      property :at_time, predicate: ::RDF::PROV.atTime, multiple: false
-      property :description, predicate: ::RDF::DC.description, multiple: false
+      property :at_time, predicate: ::RDF::PROV.atTime, multiple: false do |index|
+        index.as :displayable
+      end
       
+      property :description, predicate: ::RDF::DC.description, multiple: false do |index|
+        index.as :displayable
+      end
       
       def max_one_original_org
         if original_organizations.size > 1
           errors.add(:original_organizations, 'can\'t have more than 1 organization.')
         end
-      end 
+      end
     end
   end
 end
---
+++ app/models/lna/organization/historic.rb
@@ -0,0 +1,22 @@
+require 'owl_time'
+module Lna
+  class Organization
+    class Historic < ActiveFedora::Base
+      include Lna::OrganizationCoreBehavior
+      
+      belongs_to :changed_by, class_name: 'Lna::Organization::ChangeEvent',
+                 predicate: ::RDF::Vocab::ORG.changedBy
+
+#      validates_presence_of :end_date
+      
+      # Serialization of sub and super organizations.
+      property :historic_placement, multiple: false,
+               predicate: ::RDF::URI('http://lna.dartmouth.edu/use#HistoricPlacement')
+
+      # end_date
+      property :end_date, predicate: Vocabs::OwlTime.hasEnd, multiple: false do |index|
+        index.as :dateable
+      end
+    end
+  end
+end
