12d84b44ebdecc9d0c7a507c3a385cb4a2070867
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-10-07 11:44:53 -0400

    Updated to collection and document models.

--- app/models/lna/collection.rb
+++ app/models/lna/collection.rb
@@ -1,15 +1,13 @@
 module Lna
   class Collection < ActiveFedora::Base
-  has_many :documents, class_name: 'Lna::Collection::Document',
-           predicate: ::RDF::DC.hasPart, dependent: :destroy
-  
-  # Probably needs a better predicate.
-  belongs_to :person, class_name: 'Lna::Person',
-             predicate: ::RDF::FOAF.publications 
-  
-  validates_presence_of :person
-  
-  type ::RDF::Vocab::BIBO.Collection
-
+    has_many :documents, class_name: 'Lna::Collection::Document',
+             predicate: ::RDF::DC.hasPart, dependent: :destroy
+    
+    belongs_to :person, class_name: 'Lna::Person',
+               predicate: ::RDF::FOAF.publications 
+    
+    validates_presence_of :person
+    
+    type ::RDF::Vocab::BIBO.Collection
   end
 end  
--- app/models/lna/collection/document.rb
+++ app/models/lna/collection/document.rb
@@ -1,8 +1,13 @@
 module Lna
   class Collection
     class Document < ActiveFedora::Base
-      has_many :documents, class_name: 'Lna::Collection::Document',
-               predicate: ::RDF::Vocab::BIBO.reviewOf
+      # is reviewed by many documents
+      has_many :reviews, class_name: 'Lna::Collection::Document',
+               inverse_of: :review_of, as: :review_of
+
+      # reviews one document
+      belongs_to :review_of, class_name: 'Lna::Collection::Document',
+                 predicate ::RDF::Vocab::BIBO.reviewOf
       
       belongs_to :collection, class_name: 'Lna::Collection',
                  predicate: ::RDF::DC.isPartOf
@@ -11,18 +16,53 @@ module Lna
       
       type ::RDF::Vocab::BIBO.Document
       
-      property :author_list, predicate: ::RDF::Vocab::BIBO.authorList, multiple: false
-      property :publisher, predicate: ::RDF::DC.publisher, multiple: false
-      property :date, predicate: ::RDF::DC.date, multiple: false
-      property :title, predicate: ::RDF::DC.title, multiple: false
-      property :page_start, predicate: ::RDF::Vocab::BIBO.pageStart, multiple: false
-      property :page_end, predicate: ::RDF::Vocab::BIBO.pageEnd, multiple: false
-      property :pages, predicate: ::RDF::Vocab::BIBO.pages, multiple: false
-      property :volume, predicate: ::RDF::Vocab::BIBO.volume, multiple: false
-      property :issue, predicate: ::RDF::Vocab::BIBO.issue, multiple: false
-      property :number, predicate: ::RDF::Vocab::BIBO.number, multiple: false
-      property :uri_identifier, predicate: ::RDF::Vocab::BIBO.uri
-      property :doi, predicate: ::RDF::Vocab::BIBO.doi, multiple: false
+      property :author_list, predicate: ::RDF::Vocab::BIBO.authorList, multiple: false do |index|
+        index.as :stored_searchable
+      end
+      
+      property :publisher, predicate: ::RDF::DC.publisher, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :date, predicate: ::RDF::DC.date, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :title, predicate: ::RDF::DC.title, multiple: false do |index|
+        index.as :stored_searchable
+      end
+      
+      property :page_start, predicate: ::RDF::Vocab::BIBO.pageStart, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :page_end, predicate: ::RDF::Vocab::BIBO.pageEnd, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :pages, predicate: ::RDF::Vocab::BIBO.pages, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :volume, predicate: ::RDF::Vocab::BIBO.volume, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :issue, predicate: ::RDF::Vocab::BIBO.issue, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :number, predicate: ::RDF::Vocab::BIBO.number, multiple: false do |index|
+        index.as :displayable
+      end
+      
+      property :uri_identifier, predicate: ::RDF::Vocab::BIBO.uri do |index|
+        index.as :displayable
+      end
+
+      property :doi, predicate: ::RDF::Vocab::BIBO.doi, multiple: false do |index|
+        index.as :stored_searchable
+      end
     end
   end
 end
