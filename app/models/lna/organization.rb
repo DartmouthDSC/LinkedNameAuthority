c8d084ea2ae34167c1060ad064316d7dae972fe3
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-10-21 12:50:17 -0400

    Added json_serialization method to serialize historic_placement of an organization.

--- app/models/lna/organization.rb
+++ app/models/lna/organization.rb
@@ -10,5 +10,32 @@ module Lna
     has_and_belongs_to_many :super_organizations, class_name: 'Lna::Organization',
                             predicate: ::RDF::Vocab::ORG.subOrganizationOf
 
+    
+    def serialize(options = {})
+      hash =
+        {
+          label:      self.label,
+          code:       self.code,
+          alt_label:  self.alt_label,
+          begin_date: self.begin_date.to_s
+        }
+
+      only = options[:only]      
+      if !only || only == :sub
+        sub_orgs = self.sub_organizations.to_a.map { |s| s.serialize(only: :sub) }
+        hash[:sub_organizations] = sub_orgs unless sub_orgs.empty?
+      end
+
+      if !only || only == :super
+        super_orgs = self.super_organizations.to_a.map { |s| s.serialize(only: :super) }
+        hash[:super_organizations] = super_orgs unless super_orgs.empty?
+      end
+      
+      hash
+    end
+
+    def json_serialization
+      JSON.generate(self.serialize)
+    end
   end
 end
