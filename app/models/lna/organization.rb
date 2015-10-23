e11cf49a28cb204cefcfc2d303ca30727017b724
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-10-23 12:14:29 -0400

    Added method to convert active organization to historic.

--- app/models/lna/organization.rb
+++ app/models/lna/organization.rb
@@ -37,5 +37,32 @@ module Lna
     def json_serialization
       JSON.generate(self.serialize)
     end
+
+    # Converts given active organization to a historic organization and deletes
+    # active organization.
+    def self.convert_to_historic(active, end_date = Date.today, changed_by = nil)
+
+      raise 'Cannot convert because organization still has accounts' if active.accounts.count > 0
+      
+      serialization = active.json_serialization
+      
+      historic = Lna::Organization::Historic.create! do |h|
+        h.memberships = active.memberships
+        h.people = active.people
+        h.resulted_from = active.resulted_from
+        h.identifier = active.identifier
+        h.alt_label = active.alt_label
+        h.label = active.label
+        h.begin_date = active.begin_date
+        h.end_date = end_date
+        h.historic_placement = serialization
+        h.changed_by = changed_by
+
+      end
+
+      active.destroy
+      historic
+    end
+    
   end
 end
