54d8ddbb6b97b806b572218d65cd966c98329550
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-11-25 16:39:37 -0500

    Beginings of ali vocab.

---
+++ app/vocabs/ali.rb
@@ -0,0 +1,23 @@
+require 'rdf'
+module Vocabs
+  class ALI < RDF::Vocabulary('http://niso.org/schemas/ali/1.0/')
+    term :license_ref,
+         comment: %(Agreement related to the use of a work. Generally includes payment requirements, if any, and terms of use and re-use-potentially including access, reproduction, adaptation, and
+distribution, among others.),
+    
+    term :free_to_read,
+         comment: %(A work that is accessible to read online without charge or authentication (including registration) to any person with access to the internet.),
+         domain: "xsd:boolean".freeze
+    
+    property :start_date,
+             domain: ["ali:free_to_read".freeze, "ali:license_ref".freeze],
+             range: "xsd:date".freeze
+    
+    property :end_date,
+             domain: ["ali:free_to_read".freeze, "ali:license_ref".freeze],             
+             range: "xsd:date".freeze
+    
+    property :uri,
+             domain: "ali:free_to_read".freeze
+  end
+end
