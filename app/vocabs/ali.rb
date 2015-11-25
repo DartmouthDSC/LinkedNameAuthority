8779e0340f871fec1e59928dd2cff3b035324832
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-11-25 16:41:48 -0500

    Added comment for URI in ali vocab

--- app/vocabs/ali.rb
+++ app/vocabs/ali.rb
@@ -1,5 +1,5 @@
 require 'rdf'
-module Vocabs
+1;95;0cmodule Vocabs
   class ALI < RDF::Vocabulary('http://niso.org/schemas/ali/1.0/')
     term :license_ref,
          comment: %(Agreement related to the use of a work. Generally includes payment requirements, if any, and terms of use and re-use-potentially including access, reproduction, adaptation, and
@@ -18,6 +18,7 @@ distribution, among others.),
              range: "xsd:date".freeze
     
     property :uri,
+             comment: %(A string of characters used to identify the name of a resource. Such identification enables interaction with representations of the resource over the web. The most common form of a URI is the uniform resource locator (URL), frequently referred to informally as a web address.),
              domain: "ali:free_to_read".freeze
   end
 end
