e998359358f2352ec97a7d9ea0f149758fd97ce2
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2015-11-30 15:06:55 -0500

    Updates to ali vocab.

--- app/vocabs/ali.rb
+++ app/vocabs/ali.rb
@@ -4,21 +4,22 @@ module Vocabs
     term :license_ref,
          comment: %(Agreement related to the use of a work. Generally includes payment requirements, if any, and terms of use and re-use-potentially including access, reproduction, adaptation, and
 distribution, among others.),
-    
+         
     term :free_to_read,
          comment: %(A work that is accessible to read online without charge or authentication (including registration) to any person with access to the internet.),
-         domain: "xsd:boolean".freeze
     
     property :start_date,
              domain: ["ali:free_to_read".freeze, "ali:license_ref".freeze],
-             range: "xsd:date".freeze
+             range: "xsd:date".freeze,
+             type: "xsd:date".freeze
     
     property :end_date,
              domain: ["ali:free_to_read".freeze, "ali:license_ref".freeze],             
-             range: "xsd:date".freeze
+             range: "xsd:date".freeze,
+             type: "xsd:date".freeze
     
     property :uri,
-             comment: %(A string of characters used to identify the name of a resource. Such identification enables interaction with representations of the resource over the web. The most common form of a URI is the uniform resource locator (URL), frequently referred to informally as a web address.),
-             domain: "ali:free_to_read".freeze
+             domain: "ali:license_ref".freeze,
+             type: "xsd:anyURI".freeze
   end
 end
