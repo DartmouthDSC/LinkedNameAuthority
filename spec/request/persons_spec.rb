2a9ae3a04d802a7853550f1fcd63b7970debe1c4
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2016-02-03 15:59:37 -0500

    Fixed persons spec

--- spec/request/persons_spec.rb
+++ spec/request/persons_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 require 'airborne'
-#require 'api_helpers'
 
 RSpec.describe "Persons API", type: :request do
   include_context 'forces https requests'
   
   describe 'GET persons/' do
     before :context do
-      get '/persons/1', {}, { content_type: 'application/ld+json' }
+      get '/persons', {}, {
+            'ACCEPT' => 'application/ld+json',
+            'CONTENT_TYPE' => 'application/ld+json'
+          }
     end
     
     it 'returns status code of 200' do
-      expect_status :success
+      expect_status(:ok)
     end
 
     it 'return content type of application/ld+json' do
-      expect_header('Content-Type', 'application/ld+json')
+      expect_header_contains('Content-Type', 'application/ld+json')
     end
   end
   
