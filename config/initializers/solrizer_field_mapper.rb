2dd27434c4b271030b2904c8641b2c0d2ab9dbf9
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2016-02-26 09:30:04 -0500

    Updated how we are overriding the PARSED_SUFFIX constant, so that we stop getting warnings.

--- config/initializers/solrizer_field_mapper.rb
+++ config/initializers/solrizer_field_mapper.rb
@@ -1,5 +1,11 @@
 require 'lna/field_mapper'
-
 Solrizer.default_field_mapper = Lna::FieldMapper.new
 
-ActiveFedora::SolrQueryBuilder::PARSED_SUFFIX = '_tesi'.freeze
+# Overriding PARSED_SUFFIX constant in ActiveFedora because :stored_searchable was overriden to
+# be a single valued field. PARSED_SUFFIX updated to reflect the change made to :stored_searchable.
+module ActiveFedora
+  module SolrQueryBuilder
+    remove_const(:PARSED_SUFFIX) if (defined?(PARSED_SUFFIX))
+    PARSED_SUFFIX = '_tesi'.freeze
+  end
+end
