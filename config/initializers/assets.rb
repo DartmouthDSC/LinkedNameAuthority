5f1fbc41c57a0b54bdfbf22ddd58716fafa2e007
novomancy <wahwho@yahoo.com>
2016-02-27 18:09:47 -0500

    Merging LNA interface with Blacklight

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,13 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += [ '*.js',
+                                                '*.scss',
+                                                '*.erb',
+                                                '*.eot',
+                                                '*.svg',
+                                                '*.ttf',
+                                                '*.woff',
+                                                '*.gif',
+                                                'lna.css']
