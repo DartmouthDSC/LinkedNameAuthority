63ae699d372997d8266931d7b8710ddb65ad2eaf
John Bell <wahwho@yahoo.com>
2016-02-28 16:40:02 -0500

    adding jquery-ui

---
+++ app/assets/javascripts/lna.js
@@ -0,0 +1,16 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require vendor/modernizr-2.8.3.min.js
+//= require vendor/jquery-1.11.2.min.js
+//= require vendor/jquery-ui.min.js
+//= require turbolinks // Not sure if this is necessary
