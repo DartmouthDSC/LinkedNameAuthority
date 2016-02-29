6076097aa90dbc448b53277b5fa79c9d187959a3
novomancy <wahwho@yahoo.com>
2016-02-29 12:07:29 -0500

    removed blacklight

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "catalog#index"
-  blacklight_for :catalog
+  root to: "application#index"
+  # blacklight_for :catalog
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
