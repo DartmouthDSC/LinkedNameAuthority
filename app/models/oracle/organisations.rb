1b62235f3ac05b6ba444bbbfb03a76d6b9ca9b99
Eric J. Bivona <EJBivona@gmail.com>
2016-03-16 14:40:41 -0400

    Initial work on loading organizations from the HR Org view.

---
+++ app/models/oracle/organisations.rb
@@ -0,0 +1,74 @@
+module Oracle
+
+  class Organizations < ActiveRecord::Base
+
+    establish_connection("oracle_#{Rails.env}".to_sym)
+
+    self.table_name = 'DARTHR.DC_ACAD_COMMONS_ORG_V'
+#   Could be organization_id...
+    self.primary_key = 'organization'
+
+#   Here are the columns in this view:
+##  ORGANIZATION	NOT NULL VARCHAR2(240)
+##  ORG_LONG_NAME	VARCHAR2(150)
+##  ORG_SHORT_CODE	VARCHAR2(150)
+##  ORG_TYPE		VARCHAR2(30)
+##  HB			VARCHAR2(80)
+##  GL_ORG_VALUE	VARCHAR2(150)
+##  DIVISION		VARCHAR2(4000 CHAR)
+##  SCHOOL		VARCHAR2(4000 CHAR)
+##  SUB_DIVISION	VARCHAR2(4000 CHAR)
+##  DEPARTMENT		VARCHAR2(4000 CHAR)
+##  UNIT		VARCHAR2(4000 CHAR)
+##  SUB_UNIT		VARCHAR2(4000 CHAR)
+##  ORG_BEGIN_DATE	NOT NULL DATE
+##  ORG_END_DATE	DATE
+##  LAST_SYSTEM_UPDATE	DATE
+##  ORGANIZATION_ID	NOT NULL NUMBER(15)
+
+
+#   Return a hash in a connonical form for the LNA ImportController.
+    def to_hash
+      
+      unless (self.organization)
+        raise ArgumentError.new("#{self.organization_id}: No Organization (#{self})")
+      end
+
+#     Our full hash.
+      hash = { :name         => self.organization,
+               :org_id       => self.organization_id,
+               :long_name    => self.org_long_name,
+               :short_code   => self.org_short_code,
+               :type         => self.org_type,
+               :hb           => self.hb,
+               :gl_org_value => self.gl_org_value,
+               :division     => self.division,
+               :school       => self.school,
+               :sub_division => self.sub_division,
+               :department   => self.department,
+               :unit         => self.unit,
+               :sub_unit     => self.sub_unit,
+               :begin_date   => self.org_begin_date,
+               :end_date     => self.org_end_date,
+               :update_date  => self.last_system_update,
+      }
+
+#     Until we build out the LNA ImportController, we ditch fields
+#     that model doesn't know about.
+####      [ :prop_id,
+####        :initials,
+####        :known_as,
+####        :rank,
+####        :primary_group
+####      ].each do |key|
+####        hash.delete(key)
+####      end
+
+#     Return our (reduced) hash.
+      return hash
+      
+    end
+
+  end
+
+end
--- app/utilities/load/organizations.rb
+++ app/utilities/load/organizations.rb
@@ -1,6 +1,88 @@
 module Load
-  class Organizations
-    def self.from_hr_view
+  class Organizations < Loader
+    HR_ORG_IMPORT_TITLE = 'Organizations from HR Org view'
+    
+    # Keys for warnings hash.
+    NEW_ORG = 'new organization'
+    SENT_EMAIL = 'Sent email'
+    
+##  ORG_TYPE  COUNT(ORG_TYPE)
+##  -------------------------
+##  TPP       2
+##  SCH       4
+##  DIV       5
+##  ACAD DIV  7
+##  SUBDIV    13
+##  SUBUNIT   99
+##  DEPT      217
+##  UNIT      348
+    
+    def self.from_hr
+      batch_load(title: HR_ORG_IMPORT_TITLE,
+                 verbose: true,
+                 throw_errors: false,
+                 emails: ENV['IMPORTER_EMAIL_NOTICES']) do |loader|
+        [ 'DIV',
+          'ACAD DIV',
+          'SCH',
+          'SUBDIV',
+          'DEPT',
+          'UNIT',
+          'SUBUNIT',
+          'TPP' ].each do |orgType|
+          Oracle::Organizations.find_by(org_type: orgType) do |org|
+            loader.into_lna(org.to_hash);
+          end
+        end
+      end
     end
+
+#   Creates or updates Lna objects for the person described by the given hash.
+#
+# @example Example of hash 
+#   lna_hash = { netid: 'd00000k',
+#                person: {
+#                          full_name:   'Carla Galarza',
+#                          given_name:  'Carla',
+#                          family_name: 'Galarza',
+#                          mbox:        'Carla.Galarza@dartmouth.edu',
+#                          homepage:    ['www.dartmouth.edu/d00000k']
+#                        },
+#                membership: {
+#                              primary: true
+#                              title: 'Programmer/Analyst',
+#                              org: {
+#                                     label: 'Library',
+#                                     alt_label: ['DLC']
+#                                     code:  'LIB'
+#                                    }
+#                            },
+#               }
+#
+# @param hash [Hash] hash containing person, account and membership info
+# @return [Lna::Person] person that was created or updated
+    def into_lna(hash = {})
+###   EJB
+      begin
+        if hash.key?(:netid) && hash[:netid]
+          into_lna_by_netid(hash[:netid], hash)
+        else
+          raise NotImplementedError, 'Can only import if netid is present.'
+        end
+      rescue NotImplementedError => e
+        add_to_errors(e.message, hash.to_s)
+        raise if throw_errors
+      rescue ArgumentError => e
+        add_to_errors(e.message, hash.to_s)
+        raise if throw_errors
+      rescue StandardError => e
+        value = (hash[:person] && hash[:person][:full_name]) ?
+                  "#{hash[:person][:full_name]}(#{hash[:netid]})" :
+                  hash[:netid]
+        add_to_errors(e.message, value)
+        raise if throw_errors
+      end  
+    end
+
   end
 end
