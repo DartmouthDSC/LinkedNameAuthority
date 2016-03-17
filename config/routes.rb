4dc55a7a5b88635229e7ca3c4f1bce14492227ed
novomancy <wahwho@yahoo.com>
2016-03-17 15:52:03 -0400

    fixed login/logout behavior

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new', as: :new_user_session
-    get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+    get '/sign_in', to: 'users/sessions#new', as: :new_user_session
+    get '/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
   
   # Persons Collection
