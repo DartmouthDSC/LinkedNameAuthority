cdeb63b787ed6bd686364b9080482995a0f6e3b5
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2016-04-14 14:20:32 -0400

    Moved where deployed applications live.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:DartmouthDSC/LinkedNameAuthority.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :deploy_to, '/usr/local/dac/LinkedNameAuthority'
+set :deploy_to, "/opt/deploy/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
