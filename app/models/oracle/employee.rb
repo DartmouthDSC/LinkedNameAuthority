c2931d52a97e6f4fa02214a0d85a92011abf1baa
Eric J. Bivona <EJBivona@gmail.com>
2016-04-26 11:33:09 -0400

    First cut at a model for the DARTHR.DC_ACAD_COMMONS_EMP_V view.

---
+++ app/models/oracle/employee.rb
@@ -0,0 +1,97 @@
+module Oracle
+  class Faculty < OracleDatabase
+    self.table_name = 'DARTHR.DC_ACAD_COMMONS_EMP_V'
+    self.primary_key = 'netid'
+
+#   Here are the columns in this view:
+##  INITIALS           VARCHAR2(6)
+##  FIRST_NAME         VARCHAR2(150)
+##  LAST_NAME          NOT NULL VARCHAR2(150)
+##  SUFFIX             VARCHAR2(30)
+##  EMAIL              VARCHAR2(256 CHAR)
+##  NETID              VARCHAR2(150)
+##  SCHOOL             VARCHAR2(4000 CHAR)
+##  TITLE              VARCHAR2(484)
+##  FACULTY_RANK       VARCHAR2(150)
+##  DEPARTMENT         NOT NULL VARCHAR2(240)
+##  DEPARTMENT_ID      NOT NULL NUMBER(15)
+##  KNOWNAS            VARCHAR2(150)
+##  PERSON_ID          NOT NULL NUMBER(10)
+##  LASTEST_START_DATE NOT NULL DATE
+##  SCHOOL_START_DATE  DATE
+##  DEPT_START_DATE    DATE
+##  PRIMARY_FLAG       VARCHAR2(1 CHAR)
+##  LAST_MODIFIED_DATE DATE
+
+#   Return a hash in a connonical form for the Lna Person Loader.
+    def to_hash
+      
+      unless (self.netid)
+        raise ArgumentError.new("#{self.last_name}: No NetID (#{self})")
+      end
+
+#     Generate a person's full name.
+      nameParts = []
+#     See if we have a first name to work with.
+      if ((firstname = self.first_name))
+#	If there are multiple parts to the first name, work through them.
+        if ((firsts = firstname.split(' ')) && firsts.count > 1)
+          firsts.each do |part|
+#	    If true the person has an initial as part of their first
+#	    name, so we want to append a period to it.
+            if (part.length == 1 && part.upcase == part)
+              part.concat('.')
+            end
+          end
+#	  Put their first name back together.
+          firstname = firsts.join(' ')
+        end
+      end
+      nameParts.push(firstname) if (firstname)
+#     The initials field includes the initial of the first name, so we
+#     skip it in constructing the full name.
+      if (self.initials[1..-1] != '')
+        nameParts.push(self.initials[1..-1].split(//).join('.') + '.')
+      end
+      nameParts.push(self.last_name)
+      nameParts.push(self.suffix) if (self.suffix)
+
+#     Our full hash.
+      hash = { :netid         => self.netid,
+               :person        => {
+                                   :given_name  => firstname,
+                                   :family_name => self.last_name,
+                                   :full_name   => nameParts.join(' '),
+                                   :mbox        => self.email,
+                                 },
+               :membership    => {
+                                   :primary => !!(self.primary_flag),
+                                   :title   => self.title,
+                                   :org     => {
+                                                 :label     => self.department,
+                                                 :alt_label => [ self.department_id ],
+                                               }
+                                 },
+               :initials      => self.initials,
+               :known_as      => self.knownas,
+               :rank          => self.faculty_rank,
+      }
+
+#     Until we build out the LNA ImportController, we ditch fields
+#     that model doesn't know about.
+      [ :initials,
+        :known_as,
+        :rank,
+        :primary_group
+      ].each do |key|
+        hash.delete(key)
+      end
+
+#     Return our (reduced) hash.
+      return hash
+      
+    end
+
+  end
+
+end
