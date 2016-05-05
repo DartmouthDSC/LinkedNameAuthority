7afe3733faf334725b944863ab463c1e1d1a5927
novomancy <wahwho@yahoo.com>
2016-05-05 14:00:06 -0400

    fix redirect on create account

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -20,7 +20,7 @@ class AccountController < CrudController
     respond_to do |f|
       f.jsonld { render :create, status: :created, location: location,
                         content_type: 'application/ld+json' }
-      f.html {redirect_to (@request_path == 'person') ? person_path(FedoraID.shorten(acnt_holder['id'])) : organization_path(FedoraID.shorten(acnt_holder['id']))}
+      f.html {redirect_to (@request_path == 'person') ? person_path(FedoraID.shorten(@account_holder.id)) : organization_path(FedoraID.shorten(@account_holder.id))}
     end
   end
 
