798944cb80a61e69f44b630c9181f7280536dd70
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2016-06-28 09:21:31 -0400

    Using scope in oracle models to create queries. Fixed not_primary query in employee models. Updated the query to load people.

--- spec/models/oracle/employee_spec.rb
+++ spec/models/oracle/employee_spec.rb
@@ -1,27 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe Oracle::Employee, type: :model, oracle: true do
-  # Scott Drysdale
-  let(:test_netid_as)    { 'd19125j' }
-  let(:test_surname_as)  { 'Drysdale' }
+RSpec.describe Oracle::Employee, type: :model do
+  describe 'querying Oracle locally', oracle: true do 
+    # Scott Drysdale
+    let(:test_netid_as)    { 'd19125j' }
+    let(:test_surname_as)  { 'Drysdale' }
+    
+    # Tom Trimarco
+    let(:test_netid_gsm)   { 'd83637s' }
+    let(:test_surname_gsm) { 'Trimarco' }
+    
+    it 'can query for first row' do
+      expect { Oracle::Employee.first }.not_to raise_error
+      expect(Oracle::Employee.first).not_to be nil
+    end
+        
+    it 'has an A+S row we know about' do
+      expect(Oracle::Employee.find_by(netid: test_netid_as).last_name).to eql(test_surname_as)
+    end
+    
+    it 'has a GSM row we know about' do
+      expect(Oracle::Employee.find_by(netid: test_netid_gsm).last_name).to eql(test_surname_gsm)
+    end
 
-  # Tom Trimarco
-  let(:test_netid_gsm)   { 'd83637s' }
-  let(:test_surname_gsm) { 'Trimarco' }
+    describe '.primary' do
+      it 'has at least one matching row' do 
+        expect(Oracle::Employee.primary.count).to be > 1
+      end
+    end
+    
+    describe '.not_primary' do
+      it 'has at least one matching row' do
+        expect(Oracle::Employee.not_primary.count).to be > 1
+      end
+    end
 
-  it 'does not throw an error' do
-    expect { Oracle::Employee.count(distinct: true) }.not_to raise_error
+    describe '.valid_title' do
+      it 'has at least one matching row' do
+        expect(Oracle::Employee.valid_title.count).to be > 1
+      end
+    end
   end
 
-  it 'has one or more rows' do
-    expect(Oracle::Employee.count(distinct: true)).to be > 0
-  end
-
-  it 'has an A+S row we know about' do
-    expect(Oracle::Employee.find_by(netid: test_netid_as).last_name).to eql(test_surname_as)
-  end
-
-  it 'has a GSM row we know about' do
-    expect(Oracle::Employee.find_by(netid: test_netid_gsm).last_name).to eql(test_surname_gsm)
+  describe '.primary' do
+    it 'contains correct query' do
+      expect(Oracle::Employee.primary.where_values_hash).to eql({'primary_flag' => ["Y", "y"]})
+    end
   end
 end
---
+++ spec/models/oracle/organization_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Oracle::Organization, type: :model do
+  describe 'querying Oracle locally', oracle: true do
+    it 'can query for first row' do
+      expect(Oracle::Organization.first).not_to be nil
+    end
+
+    describe '.type' do
+      it 'returns at least one result for each type' do
+        Oracle::Organization::ORDERED_ORG_TYPES.each do |t|
+          expect(Oracle::Organization.type(t).count).to be > 1
+        end
+      end
+    end
+
+    describe '.modified_since' do
+      it 'does not limit query if date is nil' do
+        query = Oracle::Organization.modified_since(nil)
+        expect(query.where_values).to eq []
+        expect(query.where_values_hash).to eql({})
+      end
+      
+      it 'returns error if date not a time object' do
+        expect { Oracle::Organization.modified_since(Date.today) }.to raise_error ArgumentError
+      end
+      
+      it 'returns correct number of results' do
+        expect(
+          Oracle::Organization.modified_since(Time.now - 100.years).count
+        ).to eql Oracle::Organization.count
+      end
+    end
+
+    describe '.ended' do
+      
+    end
+    
+  end
+  
+end
