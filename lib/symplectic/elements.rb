f438954d02da9cb09669e9b9a9e7765c8a708caa
Carla Galarza <Carla.M.Galarza@dartmouth.edu>
2016-07-18 10:13:08 -0400

    Adding document types, and mapping element types to types in csl.

---
+++ lib/symplectic/elements.rb
@@ -0,0 +1,27 @@
+module Symplectic
+  module Elements
+    TYPES = {
+      'artefact'             => 'artefact',
+      'book'                 => 'book',
+      'chapter'              => 'chapter',
+      'compositon'           => 'composition',
+      'conference'           => 'conference',
+      'dataset'              => 'dataset',
+      'design'               => 'design',
+      'exhibition'           => 'exhibition',
+      'figure'               => 'figure',
+      'fileset'              => 'fileset',
+      'internet-publication' => 'internet-publication',
+      'journal-article'      => 'journal-article',
+      'media'                => 'media', 
+      'other'                => 'other',
+      'patent'               => 'patent',
+      'performance'          => 'performance',
+      'poster'               => 'poster',
+      'presentation'         => 'presentation',
+      'report'               => 'report',
+      'scholarly-edition'    => 'scholarly-edition',
+      'software'             => 'software',
+      'thesis-dissertations' => 'thesis-dissertation'
+    }.freeze
+end
