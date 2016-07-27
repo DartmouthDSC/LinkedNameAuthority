bf26a804d0a65a1178b760601735dfa71e542644
John Bell <wahwho@yahoo.com>
2016-07-27 16:38:52 -0400

    added authorization tests for index page

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,5 +1,6 @@
 require 'devise'
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
---
+++ spec/support/shared_view_contexts.rb
@@ -0,0 +1,11 @@
+shared_context 'Mock view user'  do
+	before do
+		@user = double("User").as_null_object
+		allow(@user).to receive(:name).and_return('Username')
+		allow(@user).to receive(:editor?).and_return(false)
+		allow(@user).to receive(:creator?).and_return(false)
+		allow(@user).to receive(:admin?).and_return(false)
+		allow(@user).to receive(:viewer?).and_return(false)
+		allow(controller).to receive(:current_user).and_return(@user)
+	end
+end
